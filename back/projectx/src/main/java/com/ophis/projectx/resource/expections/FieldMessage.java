3245d61204ea0271ab7e2fb8126c144101ac02cd
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-09-04 09:49:57 -0300

    Exceptions configurators/creators

---
+++ back/projectx/src/main/java/com/ophis/projectx/resource/expections/FieldMessage.java
@@ -0,0 +1,11 @@
+package com.ophis.projectx.resource.expections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FieldMessage {
+    private String fieldName;
+    private String message;
+}
---
+++ back/projectx/src/main/java/com/ophis/projectx/resource/expections/StandardError.java
@@ -0,0 +1,16 @@
+package com.ophis.projectx.resource.expections;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+public class StandardError {
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
