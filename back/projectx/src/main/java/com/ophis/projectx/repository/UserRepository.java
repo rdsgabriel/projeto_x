5df9d212fc45d0a540cf888f9b797bebe0bde26f
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-09-04 09:53:00 -0300

    User Repository

---
+++ back/projectx/src/main/java/com/ophis/projectx/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ophis.projectx.repository;
+
+import com.ophis.projectx.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByName(String name);
+    UserDetails findByUsername(String username);
+
+}
