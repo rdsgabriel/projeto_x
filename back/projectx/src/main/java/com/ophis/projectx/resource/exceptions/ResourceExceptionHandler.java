81ea2acbcefa9ed1803272ad4c7e26b2f31e58a5
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-10-26 16:11:48 -0300

    Two news exception handler MethodArgumentNotValid and AccessDenied

--- back/projectx/src/main/java/com/ophis/projectx/resource/exceptions/ResourceExceptionHandler.java
+++ back/projectx/src/main/java/com/ophis/projectx/resource/exceptions/ResourceExceptionHandler.java
@@ -5,19 +5,23 @@ import com.ophis.projectx.service.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
+    private final StandardError err = new StandardError();
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
 
-        StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError(e.getMessage());
@@ -29,11 +33,35 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardError err = new StandardError();
+
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError("Database exception");
         err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<StandardError> accessDenied(AccessDeniedException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError(e.getMessage());
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> argumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Arguments Invalid");
+        err.setMessage(Arrays.toString(e.getDetailMessageArguments()));
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
