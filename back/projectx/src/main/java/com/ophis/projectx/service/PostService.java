87d6c06924eca05837ed5cf4d1719c8b7c816230
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-11-02 16:09:37 -0300

    postUpdate

--- back/projectx/src/main/java/com/ophis/projectx/service/PostService.java
+++ back/projectx/src/main/java/com/ophis/projectx/service/PostService.java
@@ -1,34 +1,40 @@
 package com.ophis.projectx.service;
 
 import com.ophis.projectx.dto.PostDTO;
-import com.ophis.projectx.dto.UserDTO;
+import com.ophis.projectx.dto.PostUpdateDTO;
 import com.ophis.projectx.entities.Post;
 import com.ophis.projectx.entities.User;
 import com.ophis.projectx.mapper.PostMapper;
 import com.ophis.projectx.repository.PostRepository;
+import com.ophis.projectx.repository.UserRepository;
 import com.ophis.projectx.service.exceptions.DatabaseException;
 import com.ophis.projectx.service.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
     private final PostMapper mapper;
 
     @Transactional
-    public PostDTO createPost(User user, PostDTO dto) {
+    public PostDTO createPost(PostDTO dto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        User loggedInUser = userRepository.findByEmail(currentPrincipalName);
         Post save = postRepository.save(mapper.toEntity(dto));
         save.setCreatedAt(LocalDateTime.now());
-        save.setUser(user);
+        save.setUser(loggedInUser);
         return mapper.toDTO(save);
     }
 
@@ -45,5 +51,24 @@ public class PostService {
         }
     }
 
-
+    @Transactional
+    public PostDTO updatePost(Long id, String body) throws Exception
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        User loggedInUser = userRepository.findByEmail(currentPrincipalName);
+        Post tweetToUpdate = postRepository.findById(id).orElse(null);
+        Post save = postRepository.save(mapper.toEntityUpdate(tweetToUpdate));
+        if(tweetToUpdate == null)
+        {
+            throw new Exception("Tweet not found");
+        }
+        if(!loggedInUser.equals(tweetToUpdate.getUser()))
+        {
+            throw new Exception("No permission to edit this tweet!");
+        }
+        tweetToUpdate.setBody(body);
+        tweetToUpdate.setCreatedAt(LocalDateTime.now());
+        return mapper.toDTO(save);
+    }
 }
